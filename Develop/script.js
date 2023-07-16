8ccd92c02370502d3791f251a5996bfcd78b944c
Michael Leblanc <mleblanc1@werfen.com>
2023-07-16 13:52:33 -0400

    added prompts to users to ask them specifics about their passwords

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,19 @@
-// Assignment code here
+function generatePassword() {
+  var passLength = prompt("Please enter the length of the password (between 8 and 128 characters):")
+  if (passLength < 8 || passLength > 128) {
+    alert("Your password is not the correct length, please try again");
+    generatePassword();
+  } else {
+    let lowercaseChar = confirm("Would you like to include lowercase letters in your password");
+    let uppercaseChar = confirm("Would you like to include uppercase letters in your password");
+    let LettersChar = confirm("Would you like to include number characters in your password");
+    let specialChar = confirm("Would you like to include special characters in your password");
+  }
+    for (let i = 0; i < passLength.length; i++) {
+      
+    }
+
+}
 
 
 // Get references to the #generate element
