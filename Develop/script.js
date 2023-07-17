bc25401e00923a209849b43d4a1670146a8f3482
Michael Leblanc <mleblanc1@werfen.com>
2023-07-16 20:45:07 -0400

    added logic to return user a random password

--- Develop/script.js
+++ Develop/script.js
@@ -8,12 +8,38 @@ function generatePassword() {
     let uppercaseChar = confirm("Would you like to include uppercase letters in your password");
     let LettersChar = confirm("Would you like to include number characters in your password");
     let specialChar = confirm("Would you like to include special characters in your password");
-  }
-    for (let i = 0; i < passLength.length; i++) {
-      
-    }
 
-}
+    let passwordArray = [];
+    let lowerCaseCharacterSet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    let upperCaseCharacterSet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    let numericCharacterSet = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    let specialCharacterSet = ["!","@","#","$","%","^","&","*","(",")","-","_","=","+","[","]","{","}","<",">",",",".","?","/",":",";","|","~"];
+
+    if (lowercaseChar) {
+      passwordArray.concat(lowerCaseCharacterSet);
+    }
+  
+    if (uppercaseChar) {
+      passwordArray.concat(upperCaseCharacterSet);
+    }
+    if (uppercaseChar) {
+      passwordArray.concat(upperCaseCharacterSet);
+    }
+    if (uppercaseChar) {
+      passwordArray.concat(upperCaseCharacterSet);
+    }
+  
+  
+      for (let i = 0; i < passLength; i++) {
+  
+      }
+  
+  }
+  }
+ 
+  
+  
+  
 
 
 // Get references to the #generate element
